7f83489872205093cb65b43f0e66997185503e8d
LeeChunHao2000 <64617939+LeeChunHao2000@users.noreply.github.com>
2021-05-29 04:52:28 +0800

    Update web3.ts

--- src/utils/web3.ts
+++ src/utils/web3.ts
@@ -20,8 +20,9 @@ import { struct } from 'superstruct'
 export const web3Config = {
   strategy: 'speed',
   rpcs: [
-    { url: 'https://solana-api.projectserum.com', weight: 50 },
-    { url: 'https://raydium.rpcpool.com', weight: 40 },
+    { url: 'https://vip-api.mainnet-beta.solana.com/', weight: 30 },
+    { url: 'https://solana-api.projectserum.com', weight: 30 },
+    { url: 'https://raydium.rpcpool.com', weight: 30 },
     { url: 'https://api.mainnet-beta.solana.com', weight: 10 }
   ]
 }
